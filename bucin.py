d0b4e49ce9f6ae2587858a47afb89872d2356b7c
mousecyber-sec <53597227+mousecyber-sec@users.noreply.github.com>
2019-09-04 16:27:27 +0700

    Add files via upload

---
+++ bucin.py
@@ -0,0 +1,55 @@
+#Jangan Recode:)
+#https://github.com/mousecyber-sec /bucin
+
+import sys
+import os
+import time
+
+ungu = '\033[34;1m'
+hijau = '\033[32;1m'
+pink = '\033[35;1m'
+biru = '\033[36;1m'
+merah = '\033[31;1m'
+putih = '\033[37;1m'
+kuning = '\033[33;1m'
+print('')
+
+pilihan = '''
+SELAMAT DATANG!!
+\033[32;1m[1] Apa itu LDR?
+\033[36;1m[2] Aku Sayang Dia:)
+\033[31;1m[3] exit'''
+os.system ('clear')
+time.sleep(1)
+print('')
+print (pilihan)
+bucin = input('\033[33;1mMau Yang Mana? :')
+print('')
+time.sleep(1)
+if bucin =='1':
+    os.system('clear')
+    print ("\033[36;1mAda yang bilang LDR itu sayang tapi jauh.")
+    time.sleep (3)
+    os.system('clear')
+    print ("Ada juga yang bilang ldr itu seru,,karena seru aja wkwkw")
+if bucin =='2':
+    time.sleep (1)
+    os.system ('clear')
+    print ("\033[36;1mKuharap kamu tidak terlalu menaruh harapan")
+    time.sleep (3)
+    os.system ('clear')
+    print ("\033[31;1mKarena tak selamanya perasaan kita dengan si dia sama")
+    time.sleep (3)
+    os.system ('clear')
+    print ("Bisa saja dia menolak cintamu:')")
+    time.sleep (3)
+    os.system ('clear')
+    print ("Dan juga dapat membuat mu sakit hati")
+    time.sleep (3)
+    os.system ('clear')
+    print ("Jadi jangan terlalu berharap:')")
+    
+
+
+else :
+    print(':)')
